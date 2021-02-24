4e61648e5ad6db625fcdb5db583c96c98986120d
remontees <remontees@laposte.net>
2021-02-24 01:10:04 +0100

    Fix useless dependencies+fix label on lines.

--- draw_graph.py
+++ draw_graph.py
@@ -3,16 +3,21 @@ Module contenant le nécessaire pour tracer un graphique de circulation.
 """
 import numpy as np
 import pandas as pd
+from math import sqrt
 from matplotlib import pyplot as plt
+from matplotlib import dates as plt_dates
 
-from utilities import time_to_num
+from utilities import time_to_timedelta, timedelta_to_time, timedelta_to_num
+from datetime import timedelta
+
 
 class DrawGraph:
     """
     Classe assurant la construction d'un graphique de circulation voulu
-    en utilisant les librairies matplotlib et pandas à partir de données déjà disponibles 
+    en utilisant les librairies matplotlib et pandas à partir de données déjà disponibles
     sous forme de dataframes Pandas.
     """
+
     def __init__(self, df_gares, df_sillons):
         """
         Constructeur de la classe permettant de dessiner et d'afficher le graphique final.
@@ -33,7 +38,6 @@ class DrawGraph:
         }
         self.fig, self.ax = plt.subplots(figsize=(20, 20))
 
-
     def pick_color(self, name):
         """
         Pick the right color of the train according to its name.
@@ -41,9 +45,9 @@ class DrawGraph:
         for type_train in self.colors.keys():
             if name.startswith(type_train):
                 return self.colors[type_train]
-        
+
         return self.default_color
-    
+
     def finalize_graph(self):
         """
         Finalise le rendu du graphique.
@@ -65,13 +69,14 @@ class DrawGraph:
                 'Heure': x_col,
                 'y': pk_col
             }
-            df_train = pd.DataFrame(data = data_train)
-            df_train['Heure'] = pd.to_datetime(df_train['Heure'], errors='coerce', format='%H:%M:%S').dt.time
+            df_train = pd.DataFrame(data=data_train)
+            df_train['Heure'] = pd.to_datetime(
+                df_train['Heure'], errors='coerce', format='%H:%M:%S').dt.time
 
             # Mask every NA value
             df_train = df_train.dropna()
             train_color = self.pick_color(label)
-            df_train.plot.line(
+            _ = df_train.plot.line(
                 x='Heure',
                 y='y',
                 label=label,
@@ -84,30 +89,65 @@ class DrawGraph:
         """
         Draws the name of the circulation along the line representing the circulation (+/-).
         """
+        def y_distance(points):
+            """
+            Retourne |dy| = |y2-y1| d'un couple de deux points.
+            """
+            (_, y1), (_, y2) = points
+
+            return abs(y2-y1)
+
         if len(df_train['Heure'].index) > 1 and len(df_train['y'].index) > 1:
-            x1 = df_train['Heure'].iloc[1]
-            x2 = df_train['Heure'].iloc[2]
-            y1 = df_train['y'].iloc[1]
-            y2 = df_train['y'].iloc[2]
+            iter_points = zip(df_train['Heure'].values, df_train['y'].values)
+            # list_points = list(map(map_points, iter_points))
+            list_points = list(iter_points)
+            # list_segments = list(zip(list_points[1:], list_points[:-1]))
+            # iter_euclidian_distances = map(y_distance, list_segments)
+            # plus_long_segment = max(
+            #     zip(iter_euclidian_distances, list_segments))
+            # print(plus_long_segment)
+            # # x1 = df_train['Heure'].iloc[1]
+            # # x2 = df_train['Heure'].iloc[2]
+            # # y1 = df_train['y'].iloc[1]
+            # # y2 = df_train['y'].iloc[2]
 
-            text = self.ax.annotate(text_label, xy=(x1, y1), xytext=(0, 0),
-                            textcoords='offset points',
-                            size=8,
-                            horizontalalignment='center',
-                            verticalalignment='bottom',
-                            color=label_color)
+            # (x1, y1), (x2, y2) = plus_long_segment[1]
+            # x1, x2 = time_to_timedelta(x1), time_to_timedelta(x2)
 
-            x1 = time_to_num(x1)
-            x2 = time_to_num(x2)
-            sp1 = self.ax.transData.transform_point((x1, y1))
-            sp2 = self.ax.transData.transform_point((x2, y2))
+            # x_mean = (x1+x2)/2+timedelta(seconds=60)
+            # y_mean = (y1+y2)/2
+            # xylabel = (timedelta_to_time(x_mean), y_mean)
 
-            rise = (sp2[1] - sp1[1])
-            run = (sp2[0] - sp1[0])
+            # sp1 = self.ax.transData.transform_point(
+            #     (timedelta_to_num(x1), y1))
+            # sp2 = self.ax.transData.transform_point(
+            #     (timedelta_to_num(x2), y2))
+            # # sp1, sp2 = (timedelta_to_num(x1), y1), (timedelta_to_num(x2), y2)
 
-            slope_degrees = np.degrees(np.arctan2(rise, run))
-            text.set_rotation(slope_degrees)
+            # rise = (sp2[1] - sp1[1])
+            # run = (sp2[0] - sp1[0])
 
+            # # dx, dy = self.ax.transData.transform_point((run, rise))
+            # # print(rise)
+            # # print(run)
+
+            # slope_degrees = np.degrees(np.arctan2(rise, run))
+
+            # if (slope_degrees > 90):
+            #     slope_degrees -= 180
+            # elif (slope_degrees < -90):
+            #     slope_degrees += 180
+
+            # print(slope_degrees)
+
+            self.ax.annotate(text_label, xy=list_points[0], xytext=(0, 0),
+                             textcoords='offset points',
+                             size=8,
+                             ha='center',
+                             va='bottom',
+                             rotation=0,
+                             color=label_color)
+            # text.set_rotation(slope_degrees)
 
     def draw_stations(self):
         """
@@ -115,7 +155,7 @@ class DrawGraph:
         """
         self.ax.set_yticks(list(self.df_gares['PK']))
         self.ax.set_yticklabels(list(self.df_gares['Gare']))
-    
+
     def show(self):
         """
         Affiche le graphique à l'écran à l'écran.
--- utilities.py
+++ utilities.py
@@ -1,12 +1,29 @@
 """
 Module comprenant des fonctions utilitaires.
 """
+import datetime
 
-def time_to_num(objtime):
+
+def timedelta_to_num(objtime):
     """
-    Function converting datetime.time objects to float numbers.
+    Function converting datetime.timedelta objects to float numbers.
     """
-    return (objtime.hour*60+objtime.minute)*60+objtime.second
+    return (objtime.days*24*3600)+objtime.seconds
+
+
+def time_to_timedelta(time_obj):
+    """
+    Function converting datetime.time objects to timedelta.
+    """
+    return datetime.datetime.combine(datetime.datetime.min, time_obj) - datetime.datetime.min
+
+
+def timedelta_to_time(timedelta_obj):
+    """
+    Function converting datetime.timedelta objects to datetime.time ones.
+    """
+    return (datetime.datetime.min + timedelta_obj).time()
+
 
 def check_xls_type(argument):
     """
